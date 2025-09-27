9daa860ac2fc4ecf58cd9a589ece699f22f1014f
Druva5555 <153449020+Druva5555@users.noreply.github.com>
2025-09-27 14:50:47 +0530

    Update ocr.js

--- ocr.js
+++ ocr.js
@@ -4,7 +4,7 @@ const Tesseract = require('tesseract.js');
 async function ocrFromBuffer(buffer) {
   const { data } = await Tesseract.recognize(buffer, 'eng');
   return { text: data.text };
-}
+}    
 
 async function ocrFromImage(imagePath) {
   const { data } = await Tesseract.recognize(imagePath, 'eng');
